eec26fe2b9dcbee9983203ace5d56238e7bfa17b
electem <vinzy@electems.com>
2021-01-06 11:30:33 +0530

    code commit for back end

---
+++ Back-End/app/Controllers/Http/UserController.js
@@ -0,0 +1,29 @@
+'use strict'
+const User = use('App/Models/User')
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with users
+ */
+class UserController {
+  /**
+   * Show a list of all users.
+   * GET users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async fetchAllUsers({ response }) {
+    const users = await User.all()
+
+    response.status(200).json({
+      users: users
+    })
+  }
+}
+
+module.exports = UserController
---
+++ Back-End/database/migrations/1609834025753_users_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UsersSchema extends Schema {
+  up() {
+    this.create('users', table => {
+      table.increments()
+      table.string('name', 80).notNullable().unique()
+      table.string('gender', 254)
+      table.string('image', 254)
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('users')
+  }
+}
+
+module.exports = UsersSchema
