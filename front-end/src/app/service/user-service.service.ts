50dfadca684222af8fc2ab9b86ce187a5ddc6515
electem <vinzy@electems.com>
2021-01-06 18:21:26 +0530

    code commit for authorization.

--- front-end/src/app/service/user-service.service.ts
+++ front-end/src/app/service/user-service.service.ts
@@ -1,7 +1,8 @@
-import { Injectable , Inject} from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
+import { Base64 } from 'js-base64';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,35 @@ import { DOCUMENT } from '@angular/common';
 export class UserService {
 
   private baseUrl: any;
+  auth = Base64.encode("API_USER:pwd12#");
+
+  authHeader = new HttpHeaders().set(
+    "Authorization",
+    'Basic ' + this.auth
+  );
 
   constructor(private http: HttpClient, @Inject(DOCUMENT) private document) {
-	this.baseUrl = this.document.location.protocol + '//' + this.document.location.hostname + ':3333';
+    this.baseUrl = this.document.location.protocol + '//' + this.document.location.hostname + ':3333';
   }
 
   public fetchUsers(): Observable<any> {
-    return this.http.get(`${this.baseUrl}` + '/api/users');
+
+    return this.http.get(`${this.baseUrl}` + '/api/users', {
+      headers: this.authHeader
+    });
   }
 
   public fetchMessageByUserId(userId: String): Observable<any> {
-    return this.http.get(`${this.baseUrl}` + '/api/messages/' + userId);
+
+    return this.http.get(`${this.baseUrl}` + '/api/messages/' + userId, {
+      headers: this.authHeader
+    });
   }
 
   public sendMessage(messageBody) {
-    return this.http.post(`${this.baseUrl}` + '/api/sendMessage', messageBody);
+
+    return this.http.post(`${this.baseUrl}` + '/api/sendMessage', messageBody, {
+      headers: this.authHeader
+    });
   }
 }
