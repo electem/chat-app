920a8123b2a7f976987281596508cef024f1d790
electem <vinzy@electems.com>
2021-01-06 13:34:00 +0530

    code commit for database connection

--- Back-End/config/database.js
+++ Back-End/config/database.js
@@ -4,6 +4,17 @@ const Env = use('Env')
 const Helpers = use('Helpers')
 
 module.exports = { 
+
+   /*
+  |--------------------------------------------------------------------------
+  | Default Connection
+  |--------------------------------------------------------------------------
+  |
+  | Connection defines the default connection settings to be used while
+  | interacting with SQL databases.
+  |
+  */
+ connection: Env.get('DB_CONNECTION', 'mysql'),
   /*
   |--------------------------------------------------------------------------
   | MySQL
