907e709533b064f72632b2c3827a7d8dec442cbe
electem <vinzy@electems.com>
2021-01-06 13:25:19 +0530

    code commit for dynamic url

--- front-end/src/app/service/user-service.service.ts
+++ front-end/src/app/service/user-service.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Injectable , Inject} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
 
 
@@ -9,18 +10,22 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  private baseUrl = 'http://localhost:3333/api/';
-  constructor( private http: HttpClient,) { }
+  private baseUrl: any;
+  constructor(private http: HttpClient, @Inject(DOCUMENT) private document) {
+    this.baseUrl = this.document.location.protocol + '//' + this.document.location.hostname + ':3333';
+  }
 
 
-   public fetchUsers(): Observable<any> {
+  public fetchUsers(): Observable<any> {
 
-     return this.http.get(`${this.baseUrl}`+'users',);
- }
+    return this.http.get(`${this.baseUrl}` + 'users',);
+  }
 
- public fetchMessageByUserId(userId: String): Observable<any> {
+  public fetchMessageByUserId(userId: String): Observable<any> {
 
-  return this.http.get(`${this.baseUrl}`+'messages/'+userId,);
-}
-
+    return this.http.get(`${this.baseUrl}` + 'messages/' + userId,);
+  }
+  sendMessage(messageBody) {
+    return this.http.post(`${this.baseUrl}` + '/api/createMessage', messageBody);
+  }
 }
